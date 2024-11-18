c5d800f4570b3b361e2a71e102a8fc4bec76bc3d
Bowhza <bogtank2003@gmail.com>
2024-11-17 18:28:51 -0700

    Add auth w/ token refresh to SDK Consumer

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
 using Movies.Contracts.Requests;
 using Refit;
 
@@ -11,7 +12,13 @@ using Refit;
 var services = new ServiceCollection();
 
 
-services.AddRefitClient<IMoviesApi>()
+services
+    .AddHttpClient()
+    .AddSingleton<AuthTokenProvider>()
+    .AddRefitClient<IMoviesApi>(s => new RefitSettings
+    {
+        AuthorizationHeaderValueGetter = async (message, token) => await s.GetRequiredService<AuthTokenProvider>().GetTokenAsync() 
+    })
     .ConfigureHttpClient(c => 
         c.BaseAddress = new Uri("https://localhost:44328"));
 
@@ -19,13 +26,20 @@ var provider = services.BuildServiceProvider();
 
 var moviesApi = provider.GetRequiredService<IMoviesApi>();
 
+var newMovie = await moviesApi.CreateMovieAsync(new CreateMovieRequest
+{
+    Title = "Star Wars: The Force Awakens",
+    YearOfRelease = 2015,
+    Genres = new[] { "Action" }
+});
+
 var request = new GetAllMoviesRequest
 {
     Title = null,
     Year = null,
     SortBy = null,
     Page = 1,
-    PageSize = 3
+    PageSize = 5
 };
 
 var movies = await moviesApi.GetAllMoviesAsync(request);
